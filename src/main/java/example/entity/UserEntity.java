442507d265918ffb2ad9856b1b3ce1120ac648bc
kbm1611 <bm1611@naver.com>
2026-03-11 11:42:49 +0900

    Entity 생성

---
+++ src/main/java/example/entity/UserEntity.java
@@ -0,0 +1,25 @@
+package example.entity;
+
+import example.dto.GameLogDto;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor @NoArgsConstructor @Data @Builder
+@Entity @Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer user_no;
+
+    @Column(length = 30, nullable = false , unique = true)
+    private String uid;
+
+    @Column(length = 30, nullable = false)
+    private String upwd;
+
+    @Column(length = 20, nullable = false ,unique = true)
+    private String uname;
+}
